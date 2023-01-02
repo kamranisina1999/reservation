709c3127ccf20ac424ca710def09c5d006581172
Sina Kamrani <kamrani.sina22@gmail.com>
2023-01-02 12:06:43 +0330

    add reservation view and serializers

---
+++ reservation/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework.serializers import ModelSerializer
+from models import HotelRoomReservation, FlightTicketReservation, ResidentialReservation
+
+
+class HotelRoomReservationsSerializer(ModelSerializer):
+    class Meta:
+        model = HotelRoomReservation
+        fields = ['id', 'user', 'hotel_room', 'number_of_guests', 'checkin', 'checkout', 'count_of_nights',
+                  'price_per_night', 'hotel', 'about', 'address', 'map_link', 'phone_number']
+
+
+class ResidentialReservationSerializer(ModelSerializer):
+    class Meta:
+        model = ResidentialReservation
+        fields = ['id', 'user', 'residential_category', 'residential', 'number_of_guests', 'checkin', 'checkout',
+                  'name', 'about', 'address', 'map_link', 'phone_number']
+
+
+class FlightTicketReservationSerializer(ModelSerializer):
+    class Meta:
+        model = FlightTicketReservation
+        fields = ['user', 'number_of_passengers', 'flight_class', 'price_for_one_passenger', 'origin', 'destination',
+                  'flight_number', 'flight_type', 'start_time', 'duration', 'capacity', 'airport', 'airline',
+                  'airplane']
--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import CreateAPIView
+
+from booking.reservation.models import FlightTicketReservation, ResidentialReservation, HotelRoomReservation
+from serializers import HotelRoomReservationsSerializer, ResidentialReservationSerializer, FlightTicketReservationSerializer
+
+
+class HotelRoomReservationDisplay(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = HotelRoomReservationsSerializer
+
+    def get_queryset(self):
+        room_id = self.kwargs['room_id']
+        return HotelRoomReservation.objects.filter(hotel_room__id=room_id).get()
+
+
+class ResidentialReservationDisplay(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ResidentialReservationSerializer
+
+    def get_queryset(self):
+        residential_id = self.kwargs['residential_id']
+        return ResidentialReservation.objects.filter(residential__id=residential_id).get()
+
+
+class FlightTicketReservationDisplay(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = FlightTicketReservationSerializer
+
+    def get_queryset(self):
+        ticket_id = self.kwargs['ticket_id']
+        return FlightTicketReservation.obejcts.filter(flight_ticket__id=ticket_id).get()
